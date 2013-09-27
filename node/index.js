6c2ba385fe13af4811ac9406f970a1c00a907d50
Roger R. Madjos <roger.madjos@gmail.com>
2013-09-27 11:39:01 +0800

    login backend

--- node/index.js
+++ node/index.js
@@ -1,11 +1,42 @@
 var io = require('socket.io').listen(8001, {log: false});
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	database : 'gotgdb',
+	user     : 'root',
+	password : '',
+});
+
+connection.connect();
 
 io.sockets.on('connection', function (socket) {
 	socket.on('auth', function (data) {
-		console.log(data.password);
-	});
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function (data) {
-		console.log(data);
+		var query = "select username from users where username='"+data.username+"' and password='"+data.password+"'";
+		connection.query(query,function(err,results) {
+			if(results.length == 0) {
+				socket.emit('auth_response', { response: 'denied' });
+			}
+			else {
+				var nquery = query + " and state=0";
+				connection.query(nquery,function(err,results) {
+					if(results.length > 0) {
+						socket.emit('auth_response', { response: 'verified' });
+						nquery = "update users set state=1 where username='"+data.username+"' and password='"+data.password+"'";
+						connection.query(nquery);
+					}
+				});
+				nquery = query + " and state=1";
+				connection.query(nquery,function(err,results) {
+					if(results.length > 0) {
+						socket.emit('auth_response', { response: 'already logged in' });
+					}
+				});
+				nquery = query + " and state=2";
+				connection.query(nquery,function(err,results) {
+					if(results.length > 0) {
+						socket.emit('auth_response', { response: 'currently in game' });
+					}
+				});
+			}
+		});
 	});
 });
